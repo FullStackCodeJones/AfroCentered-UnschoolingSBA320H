e1a21cb5c8fc6c325352932bd3439686a79c6cfa
FullStackCodeJones <fullstackcodejones@gmail.com>
2024-12-19 10:31:08 -0500

    Initial Commit of AfroCentered Unschooling SBA 320H

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+function App() {
+  return (
+    <div className="bg-blue-500 text-white p-5">
+      <h1 className="text-2xl font-bold">
+        Welcome to Unschooling Afrocentered
+      </h1>
+      <p className="text-lg">This is a test of Tailwind CSS integration.</p>
+    </div>
+  );
+}
+
+export default App;
